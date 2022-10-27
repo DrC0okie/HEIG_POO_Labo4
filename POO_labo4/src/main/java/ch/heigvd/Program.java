895d6e42a46da7cf66e61be97d76e768f9bb6d37
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-10-27 19:37:30 +0200

    Remove quickSort, added bubbleSort

--- POO_labo4/src/main/java/ch/heigvd/Program.java
+++ POO_labo4/src/main/java/ch/heigvd/Program.java
@@ -1,7 +1,6 @@
 package ch.heigvd;
 import java.lang.Math;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Program {
     private static final char ZERO_ASCII = '0', MINUS_ASCII = '-', PLUS_ASCII = '+';
@@ -9,9 +8,8 @@ public class Program {
         String[] test = new String[]{"42", "-55", "+14567"};
         int[] shuffledArray = new int[test.length];
         shuffledArray = getArgumentsToInt(test);
-        quickSort(shuffledArray, 0, shuffledArray.length);
+        bubbleSort(shuffledArray);
         System.out.println(Arrays.toString(shuffledArray));
-
     }
 
     private static int[] getArgumentsToInt(String[] str) throws RuntimeException {
@@ -41,60 +39,27 @@ public class Program {
                 "Cannot convert " + c + " to an integer value");
     }
 
-    private static final Random generator = new Random();
+    private static void bubbleSort(int[] data) {
+        if (data != null) {
+            boolean finished = false;
+            int size = data.length;
+            while (!finished) {
+                finished = true;
+                for (int i = 0; i < size - 1; ++i) {
+                    if (data[i] > data[i + 1]) {
+                        swap(data, i, i + 1);
+                        finished = false;
+                    }
+                }
+                --size;
+            }
+        }
+    }
 
     private static void swap(int[] data, int a, int b) {
         int temp = data[a];
         data[a] = data[b];
         data[b] = temp;
     }
-
-    private static int partition(int[] data, int first,
-                                 int last) {
-        int i = first - 1, j = last;
-        do {
-            do {
-                ++i;
-            } while (data[i] < data[last - 1]);
-
-            do {
-                --j;
-            } while (j > first && data[last - 1] < data[j]);
-
-            if (i >= j)
-                break;
-            swap(data, i, j);
-        } while (true);
-        swap(data, i, last - 1);
-        return i;
-    }
-
-    public static void quickSort(int[] data, int first, int last) {
-        while (first < last) {
-            //Random pivot selection
-            int pivot = first + getRandom(0, distance(first, last));
-            swap(data, last - 1, pivot);
-
-            //Pivot index of the partitioned array
-            int i = partition(data, first, last);
-
-            //Tail recursion optimization:
-            // If left part is smaller, recursion for left, iteration for right
-            if (distance(i, first) < distance(last, i)) {
-                quickSort(data, first, i);
-                first = i + 1;
-            } else {
-                quickSort(data, i + 1, last);
-                last = i;
-            }
-        }
-    }
-
-    private static int distance(int a, int b) {
-        return Math.abs(a - b);
-    }
-
-    private static int getRandom(int lower, int upper) {
-        return generator.nextInt(lower, upper);
-    }
 }
+
